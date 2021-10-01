43c880c757c4dd5eba16deb1b0566d52a06d9041
Joseph Kato <joseph@jdkato.io>
2021-10-01 16:19:22 -0700

    FindAllStringSubmatchIndex

--- std.go
+++ std.go
@@ -104,3 +104,42 @@ func (re *Regexp) FindAllStringSubmatch(s string, n int) [][]string {
 
 	return result
 }
+
+// FindAllStringSubmatchIndex is the 'All' version of
+// FindStringSubmatchIndex; it returns a slice of all successive matches of
+// the expression, as defined by the 'All' description in the package
+// comment.
+// A return value of nil indicates no match.
+func (re *Regexp) FindAllStringSubmatchIndex(s string, n int) [][]int {
+	m, err := re.FindStringMatch(s)
+	if err != nil {
+		panic(err)
+	}
+
+	var result [][]int
+	for m != nil {
+		m.populateOtherGroups()
+		subs := make([]int, 0, len(m.otherGroups)+1)
+		subs = append(subs, m.Group.Index)
+		for i := 0; i < len(m.otherGroups); i++ {
+			subs = append(subs, (&m.otherGroups[i]).Index)
+		}
+		result = append(result, subs)
+
+		m, err = re.FindNextMatch(m)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return result
+}
+
+// SubexpNames returns the names of the parenthesized subexpressions
+// in this StdRegexp. The name for the first sub-expression is names[1],
+// so that if m is a match slice, the name for m[i] is SubexpNames()[i].
+// Since the StdRegexp as a whole cannot be named, names[0] is always
+// the empty string. The slice should not be modified.
+func (re *Regexp) SubexpNames() []string {
+	return re.GetGroupNames()
+}
